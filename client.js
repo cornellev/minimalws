68ce50227f39d3fa02d679abb3c3d73f98310689
Jason Klein <jklein.64@verizon.net>
2023-03-04 15:38:30 -0500

    Set up client dashboard and protocol

--- client.js
+++ client.js
@@ -1,5 +1,9 @@
 const socket = new WebSocket("ws:" + location.host + location.pathname)
 
+const lap = document.getElementById("lap")
+const watts = document.getElementById("watts")
+const throttle = document.getElementById("throttle")
+
 socket.addEventListener("open", () => {
     socket.send("Hi. I'm the client, and I have connected!")
 })
@@ -9,6 +13,38 @@ socket.addEventListener("message", e => {
     const message = e.data
     console.log("> " + message)
 
-    if (message === "ping")
-        socket.send("pong")
+    // this is where you would want to define some sort 
+    // of typed JSON protocol instead of the strings that
+    // I'm using here. A JSON protocol might have a "type"
+    // key that says what to do, and should be well-documented.
+    // It only depends on the client and server!
+
+    // the current protocol is that message is one of
+    //  - ping
+    //  - lap
+    //  - watts [number]
+    //  - throttle [number]
+
+    const command = message.split(" ")
+    switch (command[0]) {
+        case "ping":
+            socket.send("pong")
+            break
+
+        case "lap":
+            // increment the lap counter
+            const current = parseInt(lap.textContent)
+            lap.textContent = current + 1
+            break
+
+        case "watts":
+        case "throttle":
+            // set wattage/throttle to the given value
+            watts.textContent = command[1]
+            break
+
+        default:
+            // error if we get something we don't understand
+            console.error(`Unknown command "${command[0]}" with parameters [${command.slice(1)}]`)
+    }
 })
