8c6ad19d8d5a8bd230f712e5cb5ce2aba6d6e941
Jason Klein <jklein.64@verizon.net>
2023-03-04 14:57:37 -0500

    Make server

--- server.js
+++ server.js
@@ -1,22 +1,39 @@
 import express from "express"
 import morgan from "morgan"
 import path from "path"
-import { createServer } from "http"
 import { WebSocketServer } from "ws"
 
 const app = express()
-// this is per the ws docs
-const server = createServer(app)
+const server = new WebSocketServer({ server: app })
 
 // log all requests to the server
 app.use(morgan("dev"))
 
-// serve the html page
-app.get("/", (_, response) =>
-    response.sendFile(path.resolve("index.html")))
+// note: don't do this in production. it exposes all
+// of the files in this directory! Convenient here though
+app.get("*", (request, response) => {
+    let filepath = path.relative("/", request.url)
+    if (filepath === "") filepath = "index.html"
+    response.sendFile(path.resolve(filepath))
+})
+
+server.on("connection", socket => {
+    // send socket errors to the main error stream
+    socket.on("error", console.error)
+
+    // print when we receive messages
+    socket.on("message", data => console.log)
+
+    // ping pong woohoo
+    socket.send("ping")
+})
+
+server.on("listening", () => {
+    console.log("Something connected to the WebSocket server!")
+})
 
 const port = process.env.PORT ?? 8000
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}!`)
     console.log("---")
 })
